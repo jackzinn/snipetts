9cd361f255c176e1645baebe08d1d49168e3a60a
Kelvin <kelvin.musselli@iv2.com.br>
2019-05-17 11:55:55 -0300

    ok

--- formatTime.js
+++ formatTime.js
@@ -1,73 +1,73 @@
 function formatTime (timeString) {
-
-    let timeStringOnlyNumbers = ''
-    let timeFormatted = '';
-    let splittedString = '';
-    let hourFourDigits = '';
-    
-    if (typeof timeString !== 'string') {
-        return 'O parâmetro tempo precisa ser uma string.';
+	let timeStringOnlyNumbers = '';
+	let timeFormatted = '';
+	let splittedString = '';
+	let hourFourDigits = '';
+	
+	if (typeof timeString !== 'string') {
+		throw 'O parâmetro precisa ser uma string.';
     }
 
-    timeStringOnlyNumbers = timeString.replace(/\D/g, '');
+	timeStringOnlyNumbers = timeString.replace(/\D/g, '');
 
-    switch (timeStringOnlyNumbers.length) {
-        case 0:
-            timeFormatted = '';
-            break;
+	switch (timeStringOnlyNumbers.length) {
+		case 0:
+			timeFormatted = '';
+			break;
 
         case 1:
-            timeFormatted = '0' + timeStringOnlyNumbers + ':00';
-            break;
+			timeFormatted = '0' + timeStringOnlyNumbers + ':00';
+			break;
 
         case 2:
-            if (parseInt(timeStringOnlyNumbers) > 24) {
-                timeFormatted = '';
-                break;
+			if (parseInt(timeStringOnlyNumbers) > 24) {
+				timeFormatted = '';
+				break;
             }
 
             if (timeStringOnlyNumbers === '24') {
-                timeFormatted = '00:00';
-                break;
+				timeFormatted = '00:00';
+				break;
             }
-
-            
-            timeFormatted = timeStringOnlyNumbers + ':00';
-            break;
+			
+			timeFormatted = timeStringOnlyNumbers + ':00';
+			break;
 
         case 3:
-            splittedString = timeStringOnlyNumbers.split('');
+			splittedString = timeStringOnlyNumbers.split('');
 
-            if (parseInt(splittedString[1] + splittedString[2]) > 59) {
-                splittedString[1] = '5';
-                splittedString[2] = '9';
-            }
+			if (parseInt(splittedString[1] + splittedString[2]) > 59) {
+            	splittedString[1] = '5';
+				splittedString[2] = '9';
+			}
 
-            timeFormatted = '0' + splittedString[0] + ':' + splittedString[1] + splittedString[2];
-            break;
+			timeFormatted = '0' + splittedString[0] + ':' + splittedString[1] + splittedString[2];
+			break;
 
         default:
-            hourFourDigits = timeStringOnlyNumbers.substr(0, 4);
-            splittedString = hourFourDigits.split('');
+			hourFourDigits = timeStringOnlyNumbers.substr(0, 4);
+			splittedString = hourFourDigits.split('');
 
-            if (parseInt(splittedString[0] + splittedString[1]) > 24) {
-                timeFormatted = '';
-                break;
-            }
+			if (parseInt(splittedString[0] + splittedString[1]) > 24) {
+            	timeFormatted = '';
+				break;
+			}
 
-            if (splittedString[0] + splittedString[1] === '24') {
-                splittedString[0] = '0';
-                splittedString[1] = '0';
+			if (splittedString[0] + splittedString[1] === '24') {
+				splittedString[0] = '0';
+				splittedString[1] = '0';
             }
-            
+			
             if (parseInt(splittedString[2] + splittedString[3]) > 59) {
-                splittedString[2] = '5';
-                splittedString[3] = '9';
-            }
+            	splittedString[2] = '5';
+				splittedString[3] = '9';
+			}
 
-            timeFormatted = splittedString[0] + splittedString[1] + ':' + splittedString[2] + splittedString[3];
-            break;
+			timeFormatted = splittedString[0] + splittedString[1] + ':' + splittedString[2] + splittedString[3];
+			break;
     }
 
     return timeFormatted;
 };
+
+formatTime(2);
